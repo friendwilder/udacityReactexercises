4db01beb7b8c8146f47b035804af7629807a9cbd
friendwilder <friendwilder@yahoo.com.mx>
2020-12-13 14:10:25 -0600

    Create ListUsers.js

---
+++ ListUsers.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+
+
+class ListUsers extends Component {
+  render() {
+    console.log(this.props)
+    //let users = Object.keys(this.props.users)
+    //console.log(users)
+    return(
+      <ol className='users-list'>
+      {this.props.users.map((user) => (
+      	<li key={user.id} className='user-list-item'>
+			<div className='user-info'>
+			<p> {user.id} {user.name} </p>
+			<p> Favorite Movie ID: {this.props.profiles.map( (profile) => {
+              if (profile.userID === user.id.toString()){
+              	return this.props.movies.map(movie => {
+            		if (movie.id.toString() === profile.favoriteMovieID) {
+                      return movie.name
+                    } else {
+                    	return null
+                    }
+            	})
+              	//return profile.favoriteMovieID
+              } else {
+              	return null
+              }
+            }				
+                          )}</p>
+
+			</div>
+		</li>
+		))}
+      </ol>
+      
+      )
+    
+  }
+  
+}
+
+export default ListUsers;
